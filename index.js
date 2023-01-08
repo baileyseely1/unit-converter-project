0b5f44ee82fe0a289faf544e2221c5aa2a9ad1c5
Bailey Seely <baileyxdxd@gmail.com>
2023-01-08 11:45:26 -0400

    initial commit
    commit main files

---
+++ index.js
@@ -0,0 +1,36 @@
+const inputEl = document.getElementById("input-field")
+const convertBtnEl = document.getElementById("convert-btn-el")
+const lengthEl = document.getElementById("length-paragraph")
+const volumeEl = document.getElementById("volume-paragraph")
+const massEl = document.getElementById("mass-paragraph")
+
+ convertBtnEl.addEventListener("click", function renderConversions() {
+    // grab value in input field and define conversion calculations
+    const input = inputEl.value
+    const meterToFeet = input * 3.281 
+    const feetToMeter = input / 3.281
+    const litersToGallons = input * 0.264
+    const gallonsToLiters = input / 0.264
+    const kilosToPounds = input * 2.204
+    const poundsToKilos = input / 2.204
+
+    // manipulate dom to reflect conversions and round to 2 decimal places
+    lengthEl.textContent = 
+    `${input} meters = ${meterToFeet.toFixed(2)} feet 
+    |
+     ${input} feet = ${feetToMeter.toFixed(2)} meters`
+
+    volumeEl.textContent = 
+    `${input} liters = ${litersToGallons.toFixed(2)} liters 
+    |
+     ${input} gallons = ${gallonsToLiters.toFixed(2)} gallons`
+     
+     massEl.textContent = 
+     `${input} kilos = ${kilosToPounds.toFixed(2)} kilos
+     |
+      ${input} pounds = ${poundsToKilos.toFixed(2)} pounds`
+
+})
+
+
+
